d62256f7eac810ca27d2a91988eaad23f5e680c6
Yi Zeng <1574692388@qq.com>
2026-04-18 15:41:00 +0800

    Update AMRE.py

--- model/block/AMRE.py
+++ model/block/AMRE.py
@@ -15,28 +15,37 @@ class DepthwiseConv1d(nn.Module):
     def forward(self, x):
         residual = x
         x = self.depthwise(x)
-        x = x + residual
+        x = x + residual 
         x = self.relu(x)
         return x
 
-class AMRE_Branch(nn.Module):
+class AMRE_Branch_Full(nn.Module):
     def __init__(self, in_features, num_heads):
         super().__init__()
         self.in_features = in_features
         self.num_heads = num_heads
-        self.split_size = in_features // 4
+        self.split_size = in_features // 4  
         
-        self.branch_3x3 = DepthwiseConv1d(self.split_size, kernel_size=3)
-        self.branch_5x5 = DepthwiseConv1d(self.split_size, kernel_size=5)
-        self.branch_7x7 = DepthwiseConv1d(self.split_size, kernel_size=7)
-        self.branch_9x9 = DepthwiseConv1d(self.split_size, kernel_size=9)
-
+        self.branch_3x3 = nn.Sequential(
+            DepthwiseConv1d(self.split_size, kernel_size=3),
+        )
+        self.branch_5x5 = nn.Sequential(
+            DepthwiseConv1d(self.split_size, kernel_size=5),
+        )
+        self.branch_7x7 = nn.Sequential(
+            DepthwiseConv1d(self.split_size, kernel_size=7),
+        )
+        self.branch_9x9 = nn.Sequential(
+            DepthwiseConv1d(self.split_size, kernel_size=9),
+        )
+   
         self.projector = nn.Conv1d(in_features, in_features, kernel_size=1)
         self.norm = nn.LayerNorm(in_features)
 
     def forward(self, x):
-        identity = x
-        
+        identity = x 
+        b, c, j = x.shape
+
         x_3x3 = x[:, :self.split_size, :]
         x_5x5 = x[:, self.split_size:2*self.split_size, :]
         x_7x7 = x[:, 2*self.split_size:3*self.split_size, :]
@@ -51,43 +60,50 @@ class AMRE_Branch(nn.Module):
         
         identity = x
         x = self.projector(x)
-        x = self.norm(x.permute(0, 2, 1)).permute(0, 2, 1)
+        x = self.norm(x.permute(0,2,1)).permute(0,2,1) 
         return identity + x
 
 class AMRE_Full(BaseModule):
-    def __init__(self, dim, num_heads, factor=4, dropout=0.1):
+    def __init__(self, 
+                 dim,          
+                 num_heads,    
+                 factor=4,     
+                 dropout=0.1): 
         super().__init__()
         self.dim = dim
         self.num_heads = num_heads
         self.factor = factor
 
         self.norm = nn.LayerNorm(dim)
-        self.gamma = nn.Parameter(torch.ones(dim) * 1e-6)
-        self.gammax = nn.Parameter(torch.ones(dim))
-        self.project1 = nn.Linear(dim, dim // factor)
-        self.project2 = nn.Linear(dim // factor, dim)
+        self.gamma = nn.Parameter(torch.ones(dim) * 1e-6) 
+        self.gammax = nn.Parameter(torch.ones(dim))         
+        self.project1 = nn.Linear(dim, dim // factor)       
+        self.project2 = nn.Linear(dim // factor, dim)       
         self.dropout = nn.Dropout(p=dropout)
 
-        self.amre_branch = AMRE_Branch(
+        self.amre_branch_full = AMRE_Branch_Full(
             in_features=dim // factor, 
             num_heads=num_heads
         )
 
     def forward(self, x):
         identity = x
-        
+        b, j, c = x.shape
+
         x = self.norm(x) * self.gamma + x * self.gammax
-        x_amre = self.project1(x)
+        x_mona = self.project1(x) 
 
-        x_amre = x_amre.permute(0, 2, 1)
-        x_amre = self.amre_branch(x_amre)
+        x_mona = x_mona.permute(0, 2, 1) 
 
-        x_amre = x_amre.permute(0, 2, 1)
-        x_amre = F.gelu(x_amre)
-        x_amre = self.dropout(x_amre)
-        x_amre = self.project2(x_amre)
+        x_mona = self.amre_branch_full(x_mona) 
 
-        return identity + x_amre
+        x_mona = x_mona.permute(0, 2, 1) 
+        x_mona = F.gelu(x_mona)
+        x_mona = self.dropout(x_mona)
+        x_mona = self.project2(x_mona)    
+
+        output = identity + x_mona
+        return output
 
 class AMRE(nn.Module):
     def __init__(self, dim, num_heads=8, factor=8):
