c6fb12f8b0ebe6c124e44db7b3e9b5dd259a5650
Yi Zeng <1574692388@qq.com>
2026-04-18 15:38:31 +0800

    Update refine.py

--- model/block/refine.py
+++ model/block/refine.py
@@ -1,94 +1,114 @@
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
-from .AMRE import AMRE
+import torch.nn.functional as F
+from mmengine.model import BaseModule
 
-class post_refine(nn.Module):
-    def __init__(self, opt, fc_unit=1024):
+class DepthwiseConv1d(nn.Module):
+    def __init__(self, in_channels, kernel_size):
         super().__init__()
+        self.depthwise = nn.Conv1d(in_channels, in_channels, 
+                                   kernel_size=kernel_size, 
+                                   groups=in_channels, 
+                                   padding=kernel_size // 2)
+        self.relu = nn.ReLU()
 
-        if opt.refine:
-           fc_unit = 1536
+    def forward(self, x):
+        residual = x
+        x = self.depthwise(x)
+        x = x + residual 
+        x = self.relu(x)
+        return x
 
-        fc_in = 3 * 2 * opt.n_joints
-        fc_out = 2 * opt.n_joints
-
-        self.pre_amre = nn.Sequential(
-            nn.Linear(fc_in, fc_unit),
-            nn.ReLU(),
-            nn.Dropout(0.1)
+class AMRE_Branch_Full(nn.Module):
+    def __init__(self, in_features, num_heads):
+        super().__init__()
+        self.in_features = in_features
+        self.num_heads = num_heads
+        self.split_size = in_features // 4  
+        
+        self.branch_3x3 = nn.Sequential(
+            DepthwiseConv1d(self.split_size, kernel_size=3),
         )
+        self.branch_5x5 = nn.Sequential(
+            DepthwiseConv1d(self.split_size, kernel_size=5),
+        )
+        self.branch_7x7 = nn.Sequential(
+            DepthwiseConv1d(self.split_size, kernel_size=7),
+        )
+        self.branch_9x9 = nn.Sequential(
+            DepthwiseConv1d(self.split_size, kernel_size=9),
+        )
+   
+        self.projector = nn.Conv1d(in_features, in_features, kernel_size=1)
+        self.norm = nn.LayerNorm(in_features)
+
+    def forward(self, x):
+        identity = x 
+        b, c, j = x.shape
+
+        x_3x3 = x[:, :self.split_size, :]
+        x_5x5 = x[:, self.split_size:2*self.split_size, :]
+        x_7x7 = x[:, 2*self.split_size:3*self.split_size, :]
+        x_9x9 = x[:, 3*self.split_size:, :]
+
+        x_3x3 = self.branch_3x3(x_3x3) 
+        x_5x5 = self.branch_5x5(x_5x5)
+        x_7x7 = self.branch_7x7(x_7x7)
+        x_9x9 = self.branch_9x9(x_9x9)
+
+        x = torch.cat([x_3x3, x_5x5, x_7x7, x_9x9], dim=1) + identity
         
-        self.amre = AMRE(dim=fc_unit, factor=2)
-        
-        self.post_amre = nn.Sequential(
-            nn.Linear(fc_unit, fc_out),
-            nn.Sigmoid()
+        identity = x
+        x = self.projector(x)
+        x = self.norm(x.permute(0,2,1)).permute(0,2,1) 
+        return identity + x
+
+class AMRE_Full(BaseModule):
+    def __init__(self, 
+                 dim,          
+                 num_heads,    
+                 factor=4,     
+                 dropout=0.5): 
+        super().__init__()
+        self.dim = dim
+        self.num_heads = num_heads
+        self.factor = factor
+
+        self.norm = nn.LayerNorm(dim)
+        self.gamma = nn.Parameter(torch.ones(dim) * 1e-6) 
+        self.gammax = nn.Parameter(torch.ones(dim))         
+        self.project1 = nn.Linear(dim, dim // factor)       
+        self.project2 = nn.Linear(dim // factor, dim)       
+        self.dropout = nn.Dropout(p=dropout)
+
+        self.amre_branch_full = AMRE_Branch_Full(
+            in_features=dim // factor, 
+            num_heads=num_heads
         )
 
-    def forward(self, x, x_1):
-        N, T, V, _ = x.size()
-        
-        x_in = torch.cat((x, x_1), -1)
-        x_in_flat = x_in.view(N, -1)
+    def forward(self, x):
+        identity = x
+        b, j, c = x.shape
 
-        x_feature = self.pre_amre(x_in_flat)
-        
-        x_amre_in = x_feature.view(N, 1, -1)
-        x_amre_out = self.amre(x_amre_in)
-        x_amre_flat = x_amre_out.view(N, -1)
-        
-        score = self.post_mona(x_amre_flat).view(N, T, V, 2)
-        
-        score_cm = Variable(torch.ones_like(score, device=x.device), requires_grad=False) - score
-        
-        x_out = x.clone()
-        x_out[:, :, :, :2] = score * x[:, :, :, :2] + score_cm * x_1[:, :, :, :2]
+        x = self.norm(x) * self.gamma + x * self.gammax
+        x_mona = self.project1(x) 
 
-        return x_out
+        x_mona = x_mona.permute(0, 2, 1) 
 
+        x_mona = self.amre_branch_full(x_mona) 
 
-def get_uvd2xyz(uvd, gt_3D, cam):
-    N, T, V, _ = uvd.size()
+        x_mona = x_mona.permute(0, 2, 1) 
+        x_mona = F.gelu(x_mona)
+        x_mona = self.dropout(x_mona)
+        x_mona = self.project2(x_mona)    
 
-    dec_out_all = uvd.view(-1, T, V, 3).clone()
-    root = gt_3D[:, :, 0, :].unsqueeze(-2).repeat(1, 1, V, 1).clone()
-    enc_in_all = uvd[:, :, :, :2].view(-1, T, V, 2).clone()
+        output = identity + x_mona
+        return output
 
-    cam_f_all = cam[..., :2].view(-1, 1, 1, 2).repeat(1, T, V, 1)
-    cam_c_all = cam[..., 2:4].view(-1, 1, 1, 2).repeat(1, T, V, 1)
-
-    z_global = dec_out_all[:, :, :, 2]
-    z_global[:, :, 0] = root[:, :, 0, 2]
-    z_global[:, :, 1:] = dec_out_all[:, :, 1:, 2] + root[:, :, 1:, 2]
-    z_global = z_global.unsqueeze(-1)
+class AMRE(nn.Module):
+    def __init__(self, dim, num_heads=8, factor=8):
+        super().__init__()
+        self.amre_full = AMRE_Full(dim=dim, num_heads=num_heads, factor=factor)
     
-    uv = enc_in_all - cam_c_all
-    xy = uv * z_global.repeat(1, 1, 1, 2) / cam_f_all
-    xyz_global = torch.cat((xy, z_global), -1)
-    
-    xyz_offset = (xyz_global - xyz_global[:, :, 0, :].unsqueeze(-2).repeat(1, 1, V, 1))
-
-    return xyz_offset
-
-
-def refine_model(model_refine, output_3D, input_2D, gt_3D, batch_cam, pad, root_joint):
-    input_2D_single = input_2D[:, pad, :, :].unsqueeze(1)
-    
-    if output_3D.size(1) > 1:
-        output_3D_single = output_3D[:, pad, :, :].unsqueeze(1)
-    else:
-        output_3D_single = output_3D
-
-    if gt_3D.size(1) > 1:
-        gt_3D_single = gt_3D[:, pad, :, :].unsqueeze(1)
-    else:
-        gt_3D_single = gt_3D
-
-    uvd = torch.cat((input_2D_single, output_3D_single[:, :, :, 2].unsqueeze(-1)), -1)
-    xyz = get_uvd2xyz(uvd, gt_3D_single, batch_cam)
-    xyz[:, :, root_joint, :] = 0
-    
-    refine_out = model_refine(output_3D_single, xyz)
-
-    return refine_out
+    def forward(self, x):
+        return self.amre_full(x)
