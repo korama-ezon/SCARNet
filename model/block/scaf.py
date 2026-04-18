2a47d437f309f8daf11001d4c98d81772f3e4e1c
Yi Zeng <1574692388@qq.com>
2026-04-18 15:35:50 +0800

    Update scaf.py

--- model/block/scaf.py
+++ model/block/scaf.py
@@ -4,20 +4,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
-from .gsa import SpatialGlobalAttention
+from .gsa import GlobalSpatialAttention
 
 
 class Gcn(nn.Module):
-    def __init__(self, in_channels, out_channels, adj):
+    def __init__(self, in_channels, out_channels, adj, keypoints, frames):
         super().__init__()
-        self.adj = nn.Parameter(adj)
-        self.kernel_size = self.adj.size(0)
+        self.adj = adj
+        self.kernel_size = self.adj.size(0) 
         self.conv = nn.Conv2d(in_channels, out_channels * self.kernel_size, kernel_size=(1, 1))
 
     def forward(self, x):
         x = self.conv(x)
         n, kc, t, v = x.size()
-        x = x.view(n, self.kernel_size, kc // self.kernel_size, t, v)
+        x = x.view(n, self.kernel_size, kc//self.kernel_size, t, v)
         x = torch.einsum('nkctv, kvw->nctw', (x, self.adj))
         return x.contiguous()
 
@@ -71,34 +71,46 @@ class Mlp_ln(nn.Module):
 
 
 class Block(nn.Module):
-    def __init__(self, length, frames, dim, tokens_dim, channels_dim, adj, drop=0.,
-                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
-        super().__init__()
-        self.frames = frames
-        
+    def __init__(self, length, frames, dim, tokens_dim, channels_dim, adj, keypoints, conv_large, gn_large, num_heads, sga_size, drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, group_layer=nn.GroupNorm):
+        super().__init__()   
         self.norm1 = norm_layer(length)
-        self.gcn_1 = Gcn(dim, dim, adj)
+        self.gcn_1 = Gcn(dim, dim, adj, keypoints, frames)
+
         self.dw_conv = nn.Sequential(
-            nn.Conv1d(dim, dim, kernel_size=7, padding=3, groups=dim),
-            nn.GroupNorm(16, dim),
-            nn.GELU()
+            nn.Conv1d(dim, dim, kernel_size=conv_large, padding=conv_large//2, groups=dim),
+            group_layer(gn_large, dim),
+            nn.GELU(),
+            nn.Conv1d(dim, dim, kernel_size=1, padding=0, groups=1)
         )
-        self.mlp_1 = Mlp(in_features=length, hidden_features=tokens_dim, act_layer=act_layer, drop=drop)
-        self.spatial_global_attn = SpatialGlobalAttention(
-            channels=dim, num_joints=length, num_heads=6, kernel_size=9
+
+        if frames == 1:
+            self.mlp_1 = Mlp(in_features=length, hidden_features=tokens_dim, act_layer=act_layer, drop=drop)
+        else:
+            self.mlp_1 = Mlp_ln(in_features=length, hidden_features=tokens_dim, act_layer=act_layer, drop=drop)
+
+        self.global_spatial_attn = GlobalSpatialAttention(
+            channels=dim, num_joints=length, num_heads=num_heads, kernel_size=sga_size, frames=frames
         )
-        self.gn_fuse = nn.GroupNorm(16, dim)
+        
+        self.gn_fuse = group_layer(gn_large, dim)
         self.act_fuse = act_layer()
 
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
         self.norm2 = norm_layer(dim)
-        self.gcn_2 = Gcn(dim, dim, adj)
+        self.gcn_2 = Gcn(dim, dim, adj, keypoints, frames)
+        
         self.mlp_2 = Mlp(in_features=dim, hidden_features=channels_dim, act_layer=act_layer, drop=drop)
+        self.a_g = nn.Parameter(torch.ones(length)) 
+        self.a_m = nn.Parameter(torch.ones(length)) 
+        self.a_s = nn.Parameter(torch.ones(length)) 
+
+        self.mha = nn.MultiheadAttention(embed_dim=512, num_heads=8, batch_first=True)
 
     def forward(self, x):
+        res = x
         x = rearrange(x, 'b j c -> b c j')
-        res_spatial = x
 
         x = self.norm1(x)
 
@@ -106,55 +118,55 @@ class Block(nn.Module):
         x_gcn_1 = self.gcn_1(x_gcn_1)
         x_gcn_1 = rearrange(x_gcn_1, 'b c 1 j -> b c j')
 
-        #MJEM
         x_mjem = self.dw_conv(x)
         x_mjem = self.mlp_1(x_mjem)
-        
-        #GSA
-        x_gsa = self.spatial_global_attn(x)
 
-        fused_spatial = self.gn_fuse(x_gsa + x_gcn_1 + x_mjem)
-        x = res_spatial + self.drop_path(self.act_fuse(fused_spatial))
+        x_sga = self.global_spatial_attn(x) 
+
+        fused_spatial = self.gn_fuse(x_sga+ x_gcn_1 + x_mjem)
+
+        x = self.drop_path(self.act_fuse(fused_spatial))
 
         x = rearrange(x, 'b c j -> b j c')
         res_channel = x
 
-        x = self.norm2(x)
-
-        x_gcn_2 = rearrange(x, 'b j c -> b c 1 j')
+        x = self.norm2(x + res)
+        mlp_output = self.mlp_2(x)
+        
+        x_gcn_2 = rearrange(mlp_output, 'b j c -> b c 1 j')
         x_gcn_2 = self.gcn_2(x_gcn_2)
         x_gcn_2 = rearrange(x_gcn_2, 'b c 1 j -> b j c')
 
-        mlp_output = self.mlp_2(x)
-
-        mlp_output_4d = mlp_output.transpose(1, 2).unsqueeze(2)
-        sca_output = self.sca_block(mlp_output_4d)
-        sca_output = sca_output.squeeze(2).transpose(1, 2)
-
-        x = res_channel + self.drop_path(sca_output + x_gcn_2)
-
-        return x
+        x = res_channel + self.drop_path(x_gcn_2)
+        
+        return x + res
 
 
-class Scaf(nn.Module):
-    def __init__(self, depth, embed_dim, channels_dim, tokens_dim, adj, drop_rate=0.1, length=17, frames=1):
+class SCAF(nn.Module):
+    def __init__(self, depth, embed_dim, channels_dim, tokens_dim, adj, keypoints, conv_large, gn_large, num_heads, sga_size, drop_rate=0.1, length=17, frames=1):
         super().__init__()
-        drop_path_rate = 0.2
+        drop_path_rate = 0.2 
         norm_layer = partial(nn.LayerNorm, eps=1e-6)
-
-        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]
+        group_layer = partial(nn.GroupNorm, eps=1e-6)
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  
 
         self.blocks = nn.ModuleList([
             Block(
-                length=length,
-                frames=frames,
-                dim=embed_dim,
-                tokens_dim=tokens_dim,
-                channels_dim=channels_dim,
-                adj=adj,
-                drop=drop_rate,
-                drop_path=dpr[i],
-                norm_layer=norm_layer
+                length=length, 
+                frames=frames, 
+                dim=embed_dim, 
+                tokens_dim=tokens_dim, 
+                channels_dim=channels_dim, 
+                adj=adj, 
+                drop=drop_rate, 
+                drop_path=dpr[i], 
+                norm_layer=norm_layer,
+                keypoints=keypoints,
+                conv_large=conv_large,
+                gn_large=gn_large,
+                num_heads=num_heads,
+                sga_size=sga_size,
+                group_layer=group_layer
             ) for i in range(depth)
         ])
 
